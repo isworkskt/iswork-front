a1294f1ee6149772f724658ff73fc6f742242bc4
mochi <57313924+BooksHorse@users.noreply.github.com>
2022-09-08 23:38:41 +0700

    login

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-import { GoogleAuthProvider } from "firebase/auth";
-import { getAuth, signInWithRedirect } from "firebase/auth";
+import { GoogleAuthProvider, type User } from "firebase/auth";
+import { getAuth, signInWithRedirect ,getRedirectResult,onAuthStateChanged } from "firebase/auth";
 import {initializeApp} from "firebase/app"
 const firebaseConfig = {
 
@@ -21,10 +21,31 @@ appId: "1:505907623169:web:72f8d25ab0874cd6111700"
 initializeApp(firebaseConfig);
 const provider = new GoogleAuthProvider();
 provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+provider.setCustomParameters({
+  hd: "sk-thonburi.ac.th"
+});
 const auth = getAuth();
+let uid:string;
 auth.useDeviceLanguage();
+let email:string;
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    // User is signed in, see docs for a list of available properties
+    // https://firebase.google.com/docs/reference/js/firebase.User
+    uid = user.uid;
+    //user =user.email;
+    email = user.email;
+    // ...
+  } else {
+    // User is signed out
+    // ...
+  }
+});
 
+//let email = auth.currentUser?.displayName
 </script>
     
-
+<h1>{auth.currentUser}</h1>
+<h1>{uid}</h1>
+{email}
 <button on:click={() => signInWithRedirect(auth, provider)}>dadad</button>
