7a212dab53680f34e4f6e61159903d28e412d980
mochi <57313924+BooksHorse@users.noreply.github.com>
2022-09-26 11:30:03 +0700

    fix

--- src/routes/api/get/+server.ts
+++
@@ -1,7 +0,0 @@
-import { error } from '@sveltejs/kit';
- 
-export async function GET({ url,platform }): Response {
-    const x = await platform.env.iswork.get('balldata');
- 
-  return new Response(String(x));
-}
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,17 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
-
+import autoprefixer from "autoprefixer";
+import postcssPresetEnv from 'postcss-preset-env';
+import cssnano from "cssnano";
+import { optimizeCss  } from "carbon-preprocess-svelte";
 const config: UserConfig = {
-	plugins: [sveltekit()],
+	plugins: [sveltekit(),process.env.NODE_ENV === "production" && optimizeCss()],
 	ssr: {
-		noExternal: ['@carbon/charts', 'carbon-components'],
+		noExternal: ['@carbon/charts', 'carbon-components']
 	},
+	css: {
+		postcss:{plugins:[autoprefixer(),postcssPresetEnv(),cssnano({"preset":"advanced"})]}
+	}
 };
 
 export default config;
